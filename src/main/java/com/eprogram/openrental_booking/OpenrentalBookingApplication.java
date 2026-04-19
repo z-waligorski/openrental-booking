1d7498a34c2bb704ac443b1544b3bce58dcfcd29
zbyszek <zbyszekwaligorski@gmail.com>
2026-04-19 09:07:42 +0200

    Initial commit

---
+++ src/main/java/com/eprogram/openrental_booking/OpenrentalBookingApplication.java
@@ -0,0 +1,13 @@
+package com.eprogram.openrental_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrentalBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenrentalBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eprogram/openrental_booking/OpenrentalBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eprogram.openrental_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrentalBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
